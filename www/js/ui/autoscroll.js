24efe2492f2348b41e80e56ed073c01f14c5bff4
Andrew Cook <ariscop@gmail.com>
2015-05-03 17:52:54 +1000

    Activate/Deactivate autoscrolling when the user scrolls

--- www/js/ui/autoscroll.js
+++ www/js/ui/autoscroll.js
@@ -2,7 +2,7 @@ qwebirc.ui.AutoScroll = new Class({
   Extends: Fx.Scroll,
   Binds: ['onScroll', 'update'],
   initialize: function(element) {
-    this.parent(element, {});
+    this.parent(element, {wheelStops: true});
     this.element.addEvent('scroll', this.onScroll);
     window.addEvent('resize', this.update);
     this.scrolling = true;
@@ -12,6 +12,21 @@ qwebirc.ui.AutoScroll = new Class({
         this.toBottom();
   },
   onScroll: function() {
-    /* TODO: activate/deactivate on scroll */
+    /* If we're at the bottom, start autoscrolling */
+    this.scrolling = this.scrolledDown();
+  },
+  scrolledDown: function() {
+    var prev = this.element.getScroll();
+    var prevbottom = this.element.getScrollSize().y;
+    var prevheight = this.element.clientHeight;
+
+    /*
+     * fixes an IE bug: the scrollheight is less than the actual height
+     * when the div isn't full
+     */
+    if(prevbottom < prevheight)
+      prevbottom = prevheight;
+
+    return prev.y + prevheight == prevbottom;
   }
 });
